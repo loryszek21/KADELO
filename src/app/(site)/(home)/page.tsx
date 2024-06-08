80942902ecee422430cb68fed8a405756067a0ba
DamiangoINF <damian.kaczmarek1998@gmail.com>
2024-06-08 15:36:55 +0200

    Home Page 1

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -2,9 +2,26 @@ import styles from "./page.module.scss";
 
 function App(): JSX.Element {
     return (
-        <>
-            HOME PAGE
-        </>
+        <div className={styles.container}>
+        <div className={styles.content}>
+            <div className={styles.logo}>
+                <code>&lt;/&gt;</code>
+            </div>
+            <div className={styles.inspiration}>
+            <p>
+                "Programming is not just a skill - it is the art of creation. Every line of code
+                is a step towards realizing your dreams. Immerse yourself in a world of possibilities,
+                where the only limitation is your imagination. Start today and create the future."
+            </p>
+            </div>
+            <div className={styles.motto}>
+                <p>"Learn to code, unlock your future"</p>
+                <div className={styles.startButtonContainer}>
+                    <button className={styles.startButton}>Start today</button>
+                </div>
+            </div>
+        </div>
+    </div>
     );
 }
 
